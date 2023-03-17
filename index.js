79aa4d4edb5f2e71f783449be4f3d530408c24dc
Saiful Islam <sa.islamb97@gmail.com>
2023-03-17 22:24:49 +0800

    initialized

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const helmet = require("helmet")
+const morgan = require("morgan")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+
+dotenv.config();
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+}
+
+
+//middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+
+
+
+app.listen(5000, ()=>{
+    console.log(`server is running at "http://localhost:5000"`)
+})
